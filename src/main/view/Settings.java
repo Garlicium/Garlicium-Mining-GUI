43b8aac1764cefd56a3014ddf7ff5174c8d22603
LITTLE-BOX\irpat <irp10@pitt.edu>
2018-02-12 07:52:37 -0500

    Put some text inside main textboxes, no longer will crash if settings file not present,

--- src/main/view/Settings.java
+++ src/main/view/Settings.java
@@ -1,5 +1,7 @@
 package main.view;
 
+import main.Main;
+
 import java.io.*;
 
 import java.util.Map;
@@ -14,19 +16,55 @@ public class Settings {
     public static Map<String, String> getSettings() {
         Map<String, String> mapObj = null;
         try {
-            FileInputStream fileIn = new FileInputStream(new File(settingsPath));
+            FileInputStream fileIn;
+            try{
+                fileIn = new FileInputStream(new File(settingsPath));
+            }catch(IOException e){
+                fixer();
+                fileIn=new FileInputStream(new File(settingsPath));
+            }
             ObjectInputStream in = new ObjectInputStream(fileIn);
-
             Object inObj = in.readObject();
             if (inObj instanceof Map) mapObj = (Map<String, String>) inObj;
             else throw new IOException(settingsPath + ": incorrect file contents");
-
             in.close();
             fileIn.close();
         } catch (IOException | ClassNotFoundException e) {
-            StacktraceAlert.create("Exception occurred", "Does Settings/Settings.ser exist?", "Exception in Settings.getSettings", e);
+            fixer();
+            try {
+                FileInputStream fileIn;
+                try{
+                    fileIn = new FileInputStream(new File(settingsPath));
+                }catch(IOException e1){
+                    fixer();
+                    fileIn=new FileInputStream(new File(settingsPath));
+                }
+                ObjectInputStream in = new ObjectInputStream(fileIn);
+                Object inObj = in.readObject();
+                if (inObj instanceof Map) mapObj = (Map<String, String>) inObj;
+                else throw new IOException(settingsPath + ": incorrect file contents");
+                in.close();
+                fileIn.close();
+            } catch (IOException | ClassNotFoundException e2) {
+                StacktraceAlert.create("Exception occurred", "Does Settings/Settings.ser exist?", "Exception in Settings.getSettings", e2);
+            }
         }
         return mapObj;
+    }
+
+    public static void fixer(){
+        try {
+            new File("Settings").mkdir();
+            new File(settingsPath).createNewFile();
+            InputStream sis = Main.class.getClassLoader().getResourceAsStream("main/settings.clean.ser");
+            OutputStream outputStream = new FileOutputStream(new File(settingsPath), false);
+            while (sis.available() != 0)
+                outputStream.write(sis.read());
+            sis.close();
+            outputStream.close();
+        }catch (IOException e){
+            StacktraceAlert.create("Exception occurred","Cannot create directory or file "+settingsPath,"Exception in Settings.fixer",e);
+        }
     }
 
     public static void setSettings(Map<String, String> mapObj) {
